4df1ed1470fb63a3a78b5643a92abf73638f8243
Rasmus Hansen <Rasmus.Hansen@LifeWorks.com>
2023-07-02 19:58:02 +0100

    ignore __wrapped__ mypy errors

--- powertools_oas_validator/overrides/validators.py
+++ powertools_oas_validator/overrides/validators.py
@@ -7,18 +7,18 @@ from openapi_core.validation.request.validators import BaseRequestValidator
 
 class CustomBaseRequestValidator(BaseRequestValidator):
     def _get_body(self, body: Optional[str], mimetype: str, operation: Spec) -> Any:
-        super()._get_body.__wrapped__(
+        super()._get_body.__wrapped__(  # type: ignore
             self, body=body, mimetype=mimetype, operation=operation
         )
 
     def _get_parameter(self, parameters: RequestParameters, param: Spec) -> Any:
-        return super()._get_parameter.__wrapped__(
+        return super()._get_parameter.__wrapped__(  # type: ignore
             self, parameters=parameters, param=param
         )
 
     def _get_security(
         self, parameters: RequestParameters, operation: Spec
     ) -> Optional[Dict[str, str]]:
-        return super()._get_security.__wrapped__(
+        return super()._get_security.__wrapped__(  # type: ignore
             self, parameters=parameters, operation=operation
         )

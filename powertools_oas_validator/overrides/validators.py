7fdf01f210550df03bda0b3c48d25eb3aec6fd6e
Rasmus Hansen <Rasmus.Hansen@LifeWorks.com>
2023-07-04 11:40:22 +0100

    remove unneeded code

--- powertools_oas_validator/overrides/validators.py
+++ powertools_oas_validator/overrides/validators.py
@@ -10,27 +10,12 @@ from openapi_core.validation.request.validators import BaseRequestValidator
 class CustomBaseRequestValidator(BaseRequestValidator):
     class CustomValidationErrorWrapper(ValidationErrorWrapper):
         ...
-        # def _raise_error(
-        #     self,
-        #     exc: OpenAPIError,
-        #     cls: OpenAPIErrorType,
-        #     f: Callable[..., Any],
-        #     *args: Any,
-        #     **kwds: Any
-        # ) -> None:
-        #     raise exc from None
 
     @CustomValidationErrorWrapper(OpenAPIError)
     def _get_body(self, body: Optional[str], mimetype: str, operation: Spec) -> Any:
         return super()._get_body.__wrapped__(  # type: ignore
             self, body=body, mimetype=mimetype, operation=operation
         )
-
-    # @CustomValidationErrorWrapper(OpenAPIError)
-    # def _get_parameter(self, parameters: RequestParameters, param: Spec) -> Any:
-    #     return super()._get_parameter.__wrapped__(  # type: ignore
-    #         self, parameters=parameters, param=param
-    #     )
 
     @CustomValidationErrorWrapper(OpenAPIError)
     def _get_security(
